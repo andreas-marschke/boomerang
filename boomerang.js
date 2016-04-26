117f6a28858e68d12fa08b02a9f3e7c84f5c4708
Andreas Marschke <andreas.marschke@gmail.com>
2016-04-26 17:15:59 +0200

    Use proper UUID

--- boomerang.js
+++ boomerang.js
@@ -338,7 +338,7 @@ BOOMR_check_doc_domain();
 		session: {
 			// You can disable all cookies by setting site_domain to a falsy value
 			domain: null,
-			ID: Math.random().toString(36).replace(/^0\./, ""),
+			ID: (function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)})(),
 			start: undefined,
 			length: 0
 		},
